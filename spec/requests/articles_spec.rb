20b8d028464f5117607d6ad3d4210c3f5f7990ef
Ramin Mammadzada <remki-design@hotmail.com>
2021-04-26 10:10:49 +0300

    Add custom rspec helper module.

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -11,19 +11,26 @@ RSpec.describe ArticlesController do
     it "should return a proper JSON" do
       article = create(:article)
       get "/articles"
-      body = JSON.parse(response.body).deep_symbolize_keys
-      expect(body).to eq(
-        data: [
-          {
-            id: article.id.to_s,
-            type: "article",
-            attributes: {
-              title: article.title,
-              content: article.content,
-              slug: article.slug
-            }
-          }
-        ]
+      expect(json_data.length).to eq(1)
+      expected = json_data.first
+      aggregate_failures do
+        expect(expected[:id]).to eq(article.id.to_s)
+        expect(expected[:type]).to eq("article")
+        expect(expected[:attributes]).to eq(
+          title: article.title,
+          content: article.content,
+          slug: article.slug
+        )
+      end
+    end
+
+    it "should return articles in proper order" do
+      older_article = create(:article, created_at: 1.hour.ago)
+      recent_article = create(:article)
+      get "/articles"
+      ids = json_data.map { |item| item[:id].to_i }
+      expect(ids).to(
+        eq([recent_article.id, older_article.id])
       )
     end
   end
